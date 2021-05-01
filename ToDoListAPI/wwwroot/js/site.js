bbcd1181f1db4288bd1bed8bd307a88a7cfde22f
phher802 <pher024@gmail.com>
2021-04-30 22:18:08 -0500

    Add deleteItem function to site.js

--- ToDoListAPI/wwwroot/js/site.js
+++ ToDoListAPI/wwwroot/js/site.js
@@ -32,3 +32,11 @@ function addItem() {
         })
         .catch(error => console.error('Unable to add item.', error));
 }
+
+function deleteItem(id) {
+    fetch('${uri}/${id}', {
+        method: 'DELETE'
+    })
+        .then(() => getItems())
+        .catch(error => console.error('Unable to delete item.', error));
+}
