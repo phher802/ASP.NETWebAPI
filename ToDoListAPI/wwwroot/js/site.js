9dc6a0fc781a414144b88763057a12d0e52f303f
phher802 <pher024@gmail.com>
2021-04-30 22:14:46 -0500

    Create site.js in js folder, and create getItems and addItem methods

---
+++ ToDoListAPI/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿const uri = 'api/TodoItems';
+let todos = [];
+
+function getItems() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => _displayitems(data))
+        .catch(error => console.error('Unable to get items.', error));
+    
+}
+
+function addItem() {
+    const addNameTextBox = document.getElementById('add-name');
+
+    const item = {
+        isComplete: false,
+        name: addNameTextBox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json())
+        .then(() => {
+            getItems();
+            addNameTextBox.value = '';
+        })
+        .catch(error => console.error('Unable to add item.', error));
+}
